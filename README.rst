cf39dd0c26b2ea314b0be308e8dd6c16d7791bbf
fh <none@none>
2019-08-22 12:10:55 +0200

    fix setup and readme

---
+++ README.rst
@@ -0,0 +1,24 @@
+Make
+====
+
+* Source Code: https://bitbucket.org/fholmer/make
+
+Summary
+-------
+
+Create project layout from jinja2 templates.
+
+Installation
+------------
+
+.. code-block:: console
+
+    $ pip install make
+
+Usage
+-----
+
+.. code-block:: console
+
+    $ python -m make project source-path target-path
+
--- setup.py
+++ setup.py
@@ -17,8 +17,9 @@ To build wheel:
 Update requirements:
     python3 -m pip freeze -r requirements.txt > requirements.txt
 
-Download required packages:
-    
+Upload to pypi:
+    python3 -m twine upload dist/*
+
 """
 
 # Always prefer setuptools over distutils
@@ -31,51 +32,27 @@ from make import __version__ as app_version
 
 here = path.abspath(path.dirname(__file__))
 
-NAME = "make"
-MAIN_PACKAGE = "make"
-
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-
-    # default setuptools options:
-    name=NAME,
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
+    name="make",
     version=app_version,
-
     description="Create project layout from jinja2 templates.",
     long_description=long_description,
-
-    # The project's main homepage.
+    keywords='make project template',
     url='https://bitbucket.org/fholmer/make',
-
-    # Author details
     author="Frode Holmer",
     author_email='fholmer+netdef@gmail.com',
-
-    # Choose your license
     license='GNU Lesser General Public License v3 or later',
-
     project_urls={
         "Source Code": "https://bitbucket.org/fholmer/make",
     },
-
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -83,11 +60,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Operating System :: Microsoft :: Windows'
     ],
-
-    keywords='Networking Monitoring',
-
     packages=find_packages(include=['make*']),
-
     install_requires=[
         'Jinja2'
     ]

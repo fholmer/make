f6a4d4d12c29ce8b25f3d53ddd864126cde2b7b0
Frode Holmer <fholmer@gmail.com>
2019-09-08 16:47:44 +0200

    tests

---
+++ tests/make/make_project/data_medium/test_base.py
@@ -0,0 +1,28 @@
+
+from unittest.mock import patch
+from make.make_project.data_medium.base import DataMediumBase
+import os
+import posixpath
+import ntpath
+
+_sep = os.path.sep
+
+def test_contain_blanks():
+    assert DataMediumBase.contains_blanks("root{0}".format(_sep))
+    assert DataMediumBase.contains_blanks("root{0}{0}dir".format(_sep))
+    assert DataMediumBase.contains_blanks("root{0}dir{0}".format(_sep))
+    assert not DataMediumBase.contains_blanks("root{0}dir".format(_sep))
+
+    with patch.object(DataMediumBase, "os_sep", posixpath.sep):
+        with patch.object(DataMediumBase, "os_sep_dbl", posixpath.sep + posixpath.sep):
+            assert DataMediumBase.contains_blanks("root/")
+            assert DataMediumBase.contains_blanks("root//dir")
+            assert DataMediumBase.contains_blanks("root/dir/")
+            assert not DataMediumBase.contains_blanks("root/dir")
+
+    with patch.object(DataMediumBase, "os_sep", ntpath.sep):
+        with patch.object(DataMediumBase, "os_sep_dbl", ntpath.sep + ntpath.sep):
+            assert DataMediumBase.contains_blanks("root\\")
+            assert DataMediumBase.contains_blanks("root\\\\dir")
+            assert DataMediumBase.contains_blanks("root\\dir\\")
+            assert not DataMediumBase.contains_blanks("root/dir")
---
+++ tests/make/make_project/data_medium/test_local.py
@@ -0,0 +1,20 @@
+import pathlib
+import pytest
+from unittest.mock import patch
+from make.make_project.data_medium.local import Local
+
+@patch(
+    'os.walk',
+     side_effect=[[
+         ("src", ["en"], []),
+         ("src/en", [], ["to", "tre"]),
+     ]]
+)
+def test_iter_filenames(walk):
+    walker = Local.iter_filenames(pathlib.Path("src"))
+    assert (1, "", None) == next(walker)
+    assert (1, "en", None) == next(walker)
+    assert (2, "en", "to") == next(walker)
+    assert (2, "en", "tre") == next(walker)
+    with pytest.raises(StopIteration):
+         next(walker)

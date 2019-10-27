21c18f36951dd92452d48d6ddec0eca538d54576
Frode Holmer <fholmer@gmail.com>
2019-10-27 12:18:29 +0100

    added url shortcuts for gitlab and github

--- make/make_get/make_get.py
+++ make/make_get/make_get.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from urllib.request import urlretrieve
+from urllib import parse
 
 from ..errors import Abort
 
@@ -8,18 +9,61 @@ def make_get(args):
     """
         Download ``source`` and store it to disk as ``target``
     """
-    source = str(args.source)
-    target = Path(args.target).absolute()
+    retrive_from_url(str(args.source), str(args.target), "")
+
+def retrive_from_url(source, target, subpath):
+    """
+        Download ``source`` and store it to disk as ``target``
+    """
+
+    uri = parse.urlsplit(source)
+
+    if uri.scheme in ("file", ""):
+        raise Abort("URI not supported: {}".format(source))
+
+    if uri.scheme == "gh":
+        # https://github.com/fholmer/make/archive/master.zip
+        uri = parse.SplitResult(
+            "https",
+            "github.com",
+            "{}/archive/master.zip".format(uri.path),
+            "",
+            ""
+        )
+        source = uri.geturl()
+
+    elif uri.scheme == "gl":
+        # https://gitlab.com/fholmer/make/-/archive/master/make-master.zip
+        # https://gitlab.com/fholmer/make/-/archive/master/make-master.zip?path=tests%2Fmake%2Fmake_project
+        if subpath:
+            subpath = parse.urlencode({"path":subpath})
+        uri = parse.SplitResult(
+            "https",
+            "gitlab.com",
+            "/{0[0]}/{0[1]}/-/archive/master/{0[1]}-master.zip".format(uri.path.split("/")),
+            subpath,
+            ""
+        )
+        source = uri.geturl()
+
+    print(repr(target))
+    if target:
+        target = Path(target).absolute()
+    else:
+        target = Path(Path(uri.path).name).absolute()
+
+
     if target.exists():
         raise Abort("target file: {} already exists".format(target))
 
     print("Download: ", source)
     print("Into    : ", target)
     urlretrieve(source, target)
+    return target
 
 
 def setup(subparsers):
     parser = subparsers.add_parser("get", help="Download source and store as target")
     parser.add_argument("source", type=str, help="source URL")
-    parser.add_argument("target", type=str, nargs="?", default=".", help="target dir")
+    parser.add_argument("target", type=str, nargs="?", default="", help="target dir")
     parser.set_defaults(func=make_get)
--- tests/make/make_get/test_make_get.py
+++ tests/make/make_get/test_make_get.py
@@ -1,15 +1,49 @@
 import argparse
 import pathlib
 from unittest.mock import Mock, patch
+import pytest
 
 from make.make_get import make_get
-
+from make import errors
 
 @patch("make.make_get.make_get.urlretrieve", return_value=Mock())
 def test_make_get(urlretrive):
-    make_get.make_get(args=argparse.Namespace(source="src", target="dst"))
-    urlretrive.assert_called_once_with("src", pathlib.Path("dst").absolute())
+    make_get.make_get(args=argparse.Namespace(source="http://src", target="dst"))
+    urlretrive.assert_called_once_with("http://src", pathlib.Path("dst").absolute())
 
+@patch("make.make_get.make_get.urlretrieve", return_value=Mock())
+def test_retrive_from_url_abort(urlretrive):
+    with pytest.raises(errors.Abort):
+        make_get.retrive_from_url("", "dst", "")
+
+@patch("make.make_get.make_get.urlretrieve", return_value=Mock())
+def test_retrive_from_url(urlretrive):
+    make_get.retrive_from_url("http://src", "dst", "")
+    urlretrive.assert_called_once_with("http://src", pathlib.Path("dst").absolute())
+
+@patch("make.make_get.make_get.urlretrieve", return_value=Mock())
+def test_retrive_from_url_github(urlretrive):
+    make_get.retrive_from_url("gh:fholmer/make", "", "")
+    urlretrive.assert_called_once_with(
+        "https://github.com/fholmer/make/archive/master.zip",
+        pathlib.Path("master.zip").absolute()
+    )
+
+@patch("make.make_get.make_get.urlretrieve", return_value=Mock())
+def test_retrive_from_url_github(urlretrive):
+    make_get.retrive_from_url("gl:fholmer/make", "", "")
+    urlretrive.assert_called_once_with(
+        "https://gitlab.com/fholmer/make/-/archive/master/make-master.zip",
+        pathlib.Path("make-master.zip").absolute()
+    )
+
+@patch("make.make_get.make_get.urlretrieve", return_value=Mock())
+def test_retrive_from_url_github(urlretrive):
+    make_get.retrive_from_url("gl:fholmer/make", "", "tests/make/make_project")
+    urlretrive.assert_called_once_with(
+        "https://gitlab.com/fholmer/make/-/archive/master/make-master.zip?path=tests%2Fmake%2Fmake_project",
+        pathlib.Path("make-master.zip").absolute()
+    )
 
 def test_setup():
 
